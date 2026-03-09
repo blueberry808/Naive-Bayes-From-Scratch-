0a7182a07cd7ba06ad53577527abe8697ee0d40d
blueberry808 <chowdmia@oregonstate.edu>
2026-03-09 16:05:31 -0700

    start to code

--- cs331-sentiment-main/classifier.py
+++ cs331-sentiment-main/classifier.py
@@ -1,5 +1,5 @@
 # This file implements a Naive Bayes Classifier
-
+import math
 
 class BayesClassifier():
     """
@@ -10,11 +10,12 @@ class BayesClassifier():
     def __init__(self):
         self.postive_word_counts = {}
         self.negative_word_counts = {}
-        self.percent_positive_scentences = 0
-        self.percent_negative_scentences = 0
+        self.percent_positive_sentences = 0
+        self.percent_negative_sentences = 0
         self.file_length = 499
         self.file_sections = [self.file_length // 4, self.file_length // 3, self.file_length // 2]
-
+        self.total_positive_sentences =0 
+        self.total_negative_sentences = 0
 
     def train(self, train_data, train_labels, vocab):
         """
@@ -22,14 +23,128 @@ class BayesClassifier():
         train_data: vectorized text
         train_labels: vectorized labels
         vocab: vocab from build_vocab
+
         """
+
+        #splitting up data into for equal parts
+        #quarter
+        p1_data = train_data[:self.file_sections[0]]
+        p1_labels = train_labels[:self.file_sections[0]]
+
+        #half 
+        p2_data = train_data[:self.file_sections[2]]
+        part2_labels = train_labels[:self.file_sections[2]]
+
+        #three quarters
+        p3_data = train_data[:3*self.file_sections[1]]
+        part3_labels = train_labels[:3*self.file_sections[1]]
+
+        #whole data
+        p4_data = train_data
+        part4_labels = train_labels
+
+        training_set = [(p1_data,p1_labels), (p2_data,part2_labels), (p3_data,part3_labels), (p4_data,part4_labels)]
+
+        for phase in range(len(training_set)): 
+            data = training_set[phase][0]
+            labels = training_set[phase][1]
+
+
+            total_positive_sentences = 0 
+            total_negative_sentences = 0 
+            total_num_sentences = len(data) 
+
+            #we need this to perform naive bayes calculations
+            for i in labels: 
+                if i ==1: 
+                    total_positive_sentences +=1
+                elif i==0: 
+                    total_negative_sentences+=1 
+
+
+            #We need to find word counts for each word in the vocab for both positive and negative sentences. This will allows us 
+            #to perform the conditional independence probabiltiy calculatiosn for inference. 
+
+            for i in range(len(data)): 
+                for j in range(len(vocab)):
+                    if data[i][j] == 1 and labels[i] == 1: 
+                        self.postive_word_counts[vocab[j]] +=1 
+                    elif data[i][j] == 1 and labels[i] == 0: 
+                        self.negative_word_counts[vocab[j]] +=1
+
+            self.total_positive_sentences = total_positive_sentences
+            self.total_negative_sentences = total_negative_sentences
+            self.percent_negative_sentences = total_negative_sentences/total_num_sentences
+            self.percent_positive_sentences = total_positive_sentences/total_num_sentences
+           
+        predictions = self.classify_text(train_data, vocab)
+        accuracy = self.accuracy(predictions, labels)
+        print("Phase ", phase, " accuracy: ", accuracy)
+
         return 1
+    
+
+    def accuracy(predicted_labels, true_labels):
+        """
+        predicted_labels: list of 0/1s predicted by classifier
+        true_labels: list of 0/1s from text file
+        return the accuracy of the predictions
+        """
+
+        #we're assuming len(pred_labels) = len(t_labels_)
+        correct = 0 
+        for i in range(len(predicted_labels)): 
+            if predicted_labels[i] == true_labels[i]: 
+                correct+=1 
+        
+        accuracy_score = correct/len(predicted_labels)
+        return accuracy_score
+
 
 
     def classify_text(self, vectors, vocab):
+
         """
         vectors: [vector1, vector2, ...]
         predictions: [0, 1, ...]
         """
+
+        predictions = [] 
+
+        total_pos_words = 0 
+        total_neg_words = 0 
+
+        for key in self.postive_word_counts:
+            total_pos_words += self.postive_word_counts[key]
+        for key in self.negative_word_counts:
+            total_neg_words += self.negative_word_counts[key]
+
+
+
+        
+        for sentence in vectors: 
+            runSumPOS += math.log(self.percent_positive_sentences)
+            runSumNEG += math.log(self.percent_negative_sentences)
+
+            for i in range(len(vocab)):     
+
+                if sentence[i] == 1:
+                        #*Check this with teacher
+                    #uniform dirichlet priors - have to add the entire vocab size to denominator since that 
+                    #accounts for all positible numerator scenarios (each word in the vocab could be present or not present in a sentence)
+                    
+                    #not sure if we're dividing correctly  here. Need to check that 
+                        #number of records dwith Y = positive. Doesn't that imply total positive sentences?
+                    runSumPOS += math.log(self.postive_word_counts[vocab[i]] +1/self.total_positive_sentences + len(vocab))
+                    runSumNEG += math.log(self.negative_word_counts[vocab[i]]/self.total_negative_sentences + len(vocab))
+
+
+            if runSumNEG>runSumPOS: 
+                predictions.append(0)
+            elif runSumPOS> runSumNEG: 
+                predictions.append(1)
+        
         return predictions
+
+
     
--- cs331-sentiment-main/sentiment.py
+++ cs331-sentiment-main/sentiment.py
@@ -1,14 +1,16 @@
 # CS331 Sentiment Analysis Assignment 3
 # This file contains the processing functions
-
-
+import re 
+from classifier import BayesClassifier
 def process_text(text):
     """
     Preprocesses the text: Remove apostrophes, punctuation marks, etc.
     Returns a list of text
     """
+    processed_text = re.sub(r'[^\w\s]','',text)
+    text_list = processed_text.split() 
 
-    return preprocessed_text
+    return text_list
 
 
 def build_vocab(preprocessed_text):
@@ -18,7 +20,16 @@ def build_vocab(preprocessed_text):
     Returns unique text tokens
     """
 
-    return vocab
+    vocab_list = []
+    for word in preprocessed_text: 
+        if word in vocab_list: 
+            continue
+        else: 
+            vocab_list.append(word)
+
+    #sort alphabetically 
+    vocab_list = sorted(vocab_list)
+    return vocab_list
 
 
 def vectorize_text(text, vocab):
@@ -28,7 +39,15 @@ def vectorize_text(text, vocab):
     vocab: vocab from build_vocab
     Returns the vectorized text and the labels
     """
-
+    size_of_vector = len(vocab) + 1 #+1 to incorporate the label 
+    vectorized_text = [0] * size_of_vector
+    for i in range(len(vocab)): 
+        if vocab[i] in text: 
+            vectorized_text[i] = 1
+        else: 
+            vectorized_text[i] = 0 
+    vectorized_text[size_of_vector] = text[len(text)] #assign the label 
+    labels = text[len(text)] #check if this is right 
     return vectorized_text, labels
 
 
@@ -39,12 +58,29 @@ def accuracy(predicted_labels, true_labels):
     return the accuracy of the predictions
     """
 
+    #we're assuming len(pred_labels) = len(t_labels_)
+    correct = 0 
+    for i in range(len(predicted_labels)): 
+        if predicted_labels[i] == true_labels[i]: 
+            correct+=1 
+    
+    accuracy_score = correct/len(predicted_labels)
     return accuracy_score
 
 
 def main():
     # Take in text files and outputs sentiment scores
+    with open("trainingSet.txt", "r", encoding="utf-8") as f: 
+        training_data = f.read()
 
+    text_list = process_text(training_data)
+    vocab = build_vocab(text_list)
+    vectors,labels = vectorize_text(text_list, vocab)
+
+    test = BayesClassifier()
+    test()
+
+    
     return 1
 
 
